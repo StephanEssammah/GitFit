a9a2595650537ed6e32341354645f526fbbe6d68
Carl Ottosson <carl.ottosson@gmail.com>
2021-11-30 12:31:56 +0100

    Login html and css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,9 +4,15 @@ const Login = () => {
   return (
     <div className="login">
         <h1 className="login__header">GitFit</h1>
-        <Link to="/">
-            <button className="btn btn--action login__button">LOGIN</button>
-        </Link>
+        <div className="login__inputs">
+          <label className="login__inputs__name" htmlFor="name">
+            <input id="name" type="text" placeholder="name" autocomplete="off" />
+            <span>Name</span>
+          </label>
+          <Link className="login__inputs__link" to="/">
+              <button className="btn btn--action login__inputs__button">LOGIN</button>
+          </Link>
+        </div>
     </div>
   );
 }
